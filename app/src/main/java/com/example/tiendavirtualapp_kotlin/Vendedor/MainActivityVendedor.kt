f59307288e5630846bb181ec2d91517e52b42c8b
Guido Choque <guido.gonext@gmail.com>
2025-11-13 00:52:31 -0500

    clase 5, NavigationDrawer

---
+++ app/src/main/java/com/example/tiendavirtualapp_kotlin/Vendedor/MainActivityVendedor.kt
@@ -0,0 +1,16 @@
+package com.example.tiendavirtualapp_kotlin.Vendedor
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.tiendavirtualapp_kotlin.R
+
+class MainActivityVendedor : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_vendedor)
+
+    }
+}
