1d0269cb5aecc93cee02f9825860a9ce03d8bba9
Guido Choque <guido.gonext@gmail.com>
2025-11-25 23:44:31 -0500

    clase 9, Registro de vendedores UI

---
+++ app/src/main/java/com/example/tiendavirtualapp_kotlin/Vendedor/RegistroVendedorActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tiendavirtualapp_kotlin.Vendedor
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tiendavirtualapp_kotlin.R
+
+class RegistroVendedorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro_vendedor)
+    }
+}
