fc4a223d854bf7478a63d02abac19564b8d5531c
Guido Choque <guido.gonext@gmail.com>
2025-12-18 23:05:35 -0500

    clase 18, NavigationDrawer y BottomNavigationBar - Cliente [P3]

--- app/src/main/java/com/example/tiendavirtualapp_kotlin/Cliente/MainActivityCliente.kt
+++ app/src/main/java/com/example/tiendavirtualapp_kotlin/Cliente/MainActivityCliente.kt
@@ -1,12 +1,76 @@
 package com.example.tiendavirtualapp_kotlin.Cliente
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
+import com.example.tiendavirtualapp_kotlin.Cliente.Bottom_Nav_Fragments_Cliente.FragmentMisOrdenesC
+import com.example.tiendavirtualapp_kotlin.Cliente.Bottom_Nav_Fragments_Cliente.FragmentTiendaC
+import com.example.tiendavirtualapp_kotlin.Cliente.Nav_Fragments_Cliente.FragmentInicioC
+import com.example.tiendavirtualapp_kotlin.Cliente.Nav_Fragments_Cliente.FragmentMiPerfilC
 import com.example.tiendavirtualapp_kotlin.R
+import com.example.tiendavirtualapp_kotlin.databinding.ActivityMainClienteBinding
+import com.google.android.material.navigation.NavigationView
 
-class MainActivityCliente : AppCompatActivity() {
+class MainActivityCliente : AppCompatActivity() , NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var binding : ActivityMainClienteBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_cliente)
+        binding = ActivityMainClienteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        binding.navigationView.setNavigationItemSelectedListener(this)
+
+        val toogle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            toolbar,
+            R.string.open_drawer,
+            R.string.close_drawer
+        )
+
+        binding.drawerLayout.addDrawerListener(toogle)
+        toogle.syncState()
+
+        replaceFragment(FragmentInicioC())
+
+    }
+
+    private fun replaceFragment(fragment : Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.navFragment, fragment)
+            .commit()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.op_inicio_c->{
+                replaceFragment(FragmentInicioC())
+            }
+            R.id.op_mi_perfil_c->{
+                replaceFragment(FragmentMiPerfilC())
+            }
+            R.id.op_cerrar_sesion_c->{
+                Toast.makeText(applicationContext, "Haz cerrado sesión", Toast.LENGTH_SHORT).show()
+            }
+            R.id.op_tienda_c->{
+                replaceFragment(FragmentTiendaC())
+            }
+            R.id.op_mis_ordenes_c->{
+                replaceFragment(FragmentMisOrdenesC())
+            }
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
--- app/src/main/java/com/example/tiendavirtualapp_kotlin/Cliente/Nav_Fragments_Cliente/FragmentInicioC.kt
+++ app/src/main/java/com/example/tiendavirtualapp_kotlin/Cliente/Nav_Fragments_Cliente/FragmentInicioC.kt
@@ -5,16 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.tiendavirtualapp_kotlin.Cliente.Bottom_Nav_Fragments_Cliente.FragmentMisOrdenesC
+import com.example.tiendavirtualapp_kotlin.Cliente.Bottom_Nav_Fragments_Cliente.FragmentTiendaC
 import com.example.tiendavirtualapp_kotlin.R
+import com.example.tiendavirtualapp_kotlin.databinding.FragmentInicioCBinding
 
 class FragmentInicioC : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inicio_c, container, false)
+    private lateinit var binding : FragmentInicioCBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentInicioCBinding.inflate(inflater,container,false)
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.op_tienda_c-> {
+                    replaceFragment(FragmentTiendaC())
+                }
+                R.id.op_mis_ordenes_c->{
+                    replaceFragment(FragmentMisOrdenesC())
+                }
+            }
+            true
+        }
+
+        replaceFragment(FragmentTiendaC())
+        binding.bottomNavigation.selectedItemId = R.id.op_tienda_c
+
+        return binding.root
+    }
+
+    private fun replaceFragment(fragment : Fragment) {
+        parentFragmentManager
+            .beginTransaction()
+            .replace(R.id.bottomFragment, fragment)
+            .commit()
     }
 
 }
