316a6d0a6ff94c01bf9df111a202b58f256cf0df
Guido Choque <guido.gonext@gmail.com>
2026-02-14 00:33:19 -0500

    clase 21, Login de Cliente UI

---
+++ app/src/main/java/com/example/tiendavirtualapp_kotlin/Cliente/LoginClienteActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tiendavirtualapp_kotlin.Cliente
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tiendavirtualapp_kotlin.R
+
+class LoginClienteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_cliente)
+    }
+}
